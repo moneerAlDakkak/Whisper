ad9f1574be2666d8cddbe895222ca7bc29644d53
moneerAlDakkak <moner.dakkak.5@gmail.com>
2021-09-14 13:34:08 +0300

    deploy

---
+++ precache-manifest.e0f567160ca37957e12bd5283243b248.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a069b84c8cda6c93fc08",
+    "url": "/Whisper/css/app.9f239657.css"
+  },
+  {
+    "revision": "4133b728d6841d90c58f",
+    "url": "/Whisper/css/chunk-21fcceff.1edd8170.css"
+  },
+  {
+    "revision": "73e399efaf14b5b8d146",
+    "url": "/Whisper/css/chunk-48909b16.c4a480fd.css"
+  },
+  {
+    "revision": "3633d22b7d7d86b4e24b",
+    "url": "/Whisper/css/chunk-5a2387e6.a0127b95.css"
+  },
+  {
+    "revision": "cae584f28bf68161a49b",
+    "url": "/Whisper/css/chunk-f20e4038.d323ecfc.css"
+  },
+  {
+    "revision": "77cc17510e3bf095550d",
+    "url": "/Whisper/css/chunk-f5bad616.2acd6747.css"
+  },
+  {
+    "revision": "61620d5b296a1e5a760aee7e9fbe3647",
+    "url": "/Whisper/icon.png"
+  },
+  {
+    "revision": "060f9a2bc246f77c7d643e6ab2349034",
+    "url": "/Whisper/img/female.060f9a2b.png"
+  },
+  {
+    "revision": "ba3e7bdc5a2186760b8f60796258e17d",
+    "url": "/Whisper/img/male.ba3e7bdc.png"
+  },
+  {
+    "revision": "30a851675b6a02cc361b6445b1ebcc8f",
+    "url": "/Whisper/img/my-ad.30a85167.jpg"
+  },
+  {
+    "revision": "eb98a23572e49d147c59b9357ad94c61",
+    "url": "/Whisper/img/weDontDoThatHere.eb98a235.jpg"
+  },
+  {
+    "revision": "7088c36679d1b69889d20eb965cd2daa",
+    "url": "/Whisper/index.html"
+  },
+  {
+    "revision": "a069b84c8cda6c93fc08",
+    "url": "/Whisper/js/app.e0339297.js"
+  },
+  {
+    "revision": "4133b728d6841d90c58f",
+    "url": "/Whisper/js/chunk-21fcceff.613a620a.js"
+  },
+  {
+    "revision": "73e399efaf14b5b8d146",
+    "url": "/Whisper/js/chunk-48909b16.f0011799.js"
+  },
+  {
+    "revision": "3633d22b7d7d86b4e24b",
+    "url": "/Whisper/js/chunk-5a2387e6.3cc22e7a.js"
+  },
+  {
+    "revision": "cae584f28bf68161a49b",
+    "url": "/Whisper/js/chunk-f20e4038.2a77f408.js"
+  },
+  {
+    "revision": "77cc17510e3bf095550d",
+    "url": "/Whisper/js/chunk-f5bad616.71f80c95.js"
+  },
+  {
+    "revision": "38df2963756295e003e8",
+    "url": "/Whisper/js/chunk-vendors.93ae4038.js"
+  },
+  {
+    "revision": "bdbd4418e450f3b5922069b487cec4c3",
+    "url": "/Whisper/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Whisper/robots.txt"
+  }
+]);
